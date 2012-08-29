7576400f625bc792d7e99c5fde39a5ba92533e89
Riley Dutton <riley@dutton.us>
2012-08-29 15:39:54 -0500

    new engine

--- js/roll20help.js
+++ js/roll20help.js
@@ -1,13 +1,20 @@
 $(function() {
 	
+	var engine = d20.dice_engine();
 	$(".diceroller").on("keyup", function(e) {
 		var $this = $(this);
 		if(e.which == 13) {
 			try{
-				var results = d20.dice_engine.process($this.find("input").val());
-				$this.find(".results").html($("#tmpl_chatmessage_rollresult").jqote(results.origformula + "|" + results.formula + "|" + results.total));
+				engine.process($this.find("input").val(), function(results) {
+					if(results.error) {
+						$(this).find(".results").html("<div class='alert alert-danger'>"+results.error+"</div>");
+						return;
+					}
+					$this.find(".results").html($("#tmpl_chatmessage_rollresult").jqote(results.origformula + "|" + results.formula + "|" + results.total));
+				});
 			}
 			catch (e) {
+				console.log(e);
 				$(this).find(".results").html("<div class='alert alert-danger'>Invalid roll! Check your formula and try again.</div>");
 			}
 			
